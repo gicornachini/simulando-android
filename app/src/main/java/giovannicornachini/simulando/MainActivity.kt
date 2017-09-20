dd7e3551fc4c9b2da096247517468b33d4b34bc2
Giovanni Cornachini <giovannicornachini@gmail.com>
2017-09-19 22:37:11 -0300

    Call question Activity

--- app/src/main/java/giovannicornachini/simulando/MainActivity.kt
+++ app/src/main/java/giovannicornachini/simulando/MainActivity.kt
@@ -2,11 +2,29 @@ package giovannicornachini.simulando
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById(R.id.startBtn) as Button
+        fab.setOnClickListener(View.OnClickListener { view ->
+            initQuestion()
+        })
+    }
+
+
+    fun initQuestion(){
+        """ Init QuestionActivity with questions. """
+        val intent = Intent(this, QuestionActivity::class.java)
+        // TODO: send questions
+        startActivity(intent)
     }
 }
